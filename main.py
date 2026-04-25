901fe92c4d1bb7640116d5b59ef0e092b2db6696
Andres Zanzani <azanzani@gmail.com>
2026-04-25 15:49:17 +0200

    chore: release 0.2.7

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ def main():
     app = QApplication(sys.argv)
     app.setApplicationName("NotePadPQ")
     app.setOrganizationName("NotePadPQ")
-    app.setApplicationVersion("0.2.6")
+    app.setApplicationVersion("0.2.7")
     
     # ... resto del file invariato ...
 

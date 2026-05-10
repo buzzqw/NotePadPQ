9fd421bd5d8f3c225be454d8467076a0820b9d6f
Andres Zanzani <azanzani@gmail.com>
2026-05-10 12:22:49 +0200

    chore: release 0.6.0

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ def main():
     app = QApplication(sys.argv)
     app.setApplicationName("NotePadPQ")
     app.setOrganizationName("NotePadPQ")
-    app.setApplicationVersion("0.5.10")
+    app.setApplicationVersion("0.6.0")
 
     # Su alcune configurazioni Linux il flag può essere True di default.
     # Lo forziamo False per garantire che le icone nei menu siano visibili.
--- ui/main_window.py
+++ ui/main_window.py
@@ -201,7 +201,7 @@ class TripleClickFilter(QObject):
 class MainWindow(QMainWindow):
 
     APP_NAME    = "NotePadPQ"
-    APP_VERSION = "0.5.10"
+    APP_VERSION = "0.6.0"
 
     def __init__(self):
         super().__init__()

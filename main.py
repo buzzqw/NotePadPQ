5ef60f80da664839dc8487bb4ba0a6e38f52be45
buzzqw <azanzani@gmail.com>
2026-05-25 14:26:40 +0200

    chore: release 1.0.9

--- main.py
+++ main.py
@@ -92,7 +92,7 @@ def main():
     app = QApplication(sys.argv)
     app.setApplicationName("NotePadPQ")
     app.setOrganizationName("NotePadPQ")
-    app.setApplicationVersion("1.0.8")
+    app.setApplicationVersion("1.0.9")
 
     # Su alcune configurazioni Linux il flag può essere True di default.
     # Lo forziamo False per garantire che le icone nei menu siano visibili.

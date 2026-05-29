ecf724238f76a06c3d3bf82891617bbe2b578b3f
buzzqw <azanzani@gmail.com>
2026-05-29 12:50:50 +0200

    chore: release 1.2.0

--- ui/main_window.py
+++ ui/main_window.py
@@ -307,7 +307,7 @@ class TripleClickFilter(QObject):
 class MainWindow(QMainWindow):
 
     APP_NAME    = "NotePadPQ"
-    APP_VERSION = "1.1.9"
+    APP_VERSION = "1.2.0"
 
     def __init__(self):
         super().__init__()

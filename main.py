e19149afc070961be04722b363239a82d97cd014
buzzqw <azanzani@gmail.com>
2026-09-15 10:34:26 +0200

    chore: release 2.0.2

--- main.py
+++ main.py
@@ -240,7 +240,7 @@ def main():
     app = app_class(sys.argv)
     app.setApplicationName("NotePadPQ")
     app.setOrganizationName("NotePadPQ")
-    app.setApplicationVersion("2.0.1")
+    app.setApplicationVersion("2.0.2")
 
     profile_log = configure_diagnostics(enabled=profile_enabled)
     if profile_log is not None:

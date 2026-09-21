4e40f3258842b995e98bbbcb0c752236dfe84d1f
buzzqw <azanzani@gmail.com>
2026-09-21 10:19:50 +0200

    chore: release 2.0.6

--- ui/main_window.py
+++ ui/main_window.py
@@ -273,7 +273,7 @@ class _ExportAsWorker(QThread):
 class MainWindow(QMainWindow):
 
     APP_NAME    = "NotePadPQ"
-    APP_VERSION = "2.0.5"
+    APP_VERSION = "2.0.6"
 
     def __init__(self):
         super().__init__()
